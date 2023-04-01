3d030d4958aa8253933071241904761dd4585e29
AlexLenk <alexlenk.ru@gmail.com>
2023-04-01 22:13:12 +0300

    Improvement footer component

--- src/components/ui/footer.jsx
+++ src/components/ui/footer.jsx
@@ -2,9 +2,12 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer>
-      <h3>2019 - {currentYear} ©</h3>
-    </footer>
+  <footer className="bg-gray-900 text-white">
+    <div className="md:flex md:justify-between md:items-center sm:px-12 px-2 py-7 mx-auto max-w-7xl">
+      2019 - {currentYear} © ООО «UI/UX developers» Все права защищены.
+      Информация на сайте не является публичной офертой согласно Ст. 437 ГК РФ
+    </div>
+  </footer>
   );
 };
 
