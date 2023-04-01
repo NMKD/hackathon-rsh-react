1feb73948dd293da715bb87f6b7bad959826b3d2
kristina <kistina@localhost>
2023-04-01 13:51:32 +0300

    added social to some users

--- src/api/users.api.js
+++ src/api/users.api.js
@@ -24,6 +24,12 @@ export const users = [
   {
     _id: "67rdca3eeb7f6fgeed471816",
     name: "Янковой Александр",
+    social: [
+      {
+        _id: "67rdca3eeb7fVK471818",
+        href: "https://vk.com/alex_lenk",
+      },
+    ],
     img: "https://sun9-8.userapi.com/impg/CKZY4xmZfF1cbghJAybzkBCm77kHaSg0m0qY0Q/CJc_jpPmsEk.jpg?size=512x512&quality=95&sign=4481422879b6d786f343ead253ea437f&type=album",
     progress: [
       { _id: "67rdca3eeb7fprogress471818", percentages: 95 },
@@ -45,6 +51,12 @@ export const users = [
   {
     _id: "67rdca3eeb7f6fgeed471817",
     name: "Карамышев Булат",
+    social: [
+      {
+        _id: "67rdca3eeb7fVK471818",
+        href: "https://vk.com/bulatsrc",
+      },
+    ],
     img: "https://avatars.githubusercontent.com/u/50518242?v=4",
     progress: [
       { _id: "67rdca3eeb7fprogress471818", percentages: 100 },
@@ -63,6 +75,12 @@ export const users = [
   {
     _id: "67rdca3eeb7f6fgeed471818",
     name: "Сенин Максим",
+    social: [
+      {
+        _id: "67rdca3eeb7telegram471818",
+        href: "https://t.me/wecpo1337",
+      },
+    ],
     img: "https://i.ibb.co/cbDcbDC/photo-2023-03-31-19-52-33.jpg",
     progress: [
       { _id: "67rdca3eeb7fprogress471818", percentages: 50 },
@@ -70,15 +88,17 @@ export const users = [
       { _id: "67rdca3eeb7progress71824", percentages: 50 },
       { _id: "67rdca3eeb7fprogress1822", percentages: 50 },
     ],
-    qualities: [
-      "67rdca3eeb7f6fgeed4729j01",
-      "67rdca3eeb7f6fgeed471101",
-      "67rdca3eeb7f6fgeed4711012",
-    ],
+    qualities: ["67rdca3eeb7sjssh1992j01", "67rdca3eeb7sjss222"],
   },
   {
     _id: "67rdca3eeb7f6fgeed471819",
     name: "Окладная Анастасия",
+    social: [
+      {
+        _id: "67rdca3eeb7telegram471818",
+        href: "https://vk.com/id71432571",
+      },
+    ],
     img: "https://sun1-24.userapi.com/s/v1/if1/iRwBwV0ukV6H9QBIWHRGziLrXSJ4T4rnKX9_shzvhmAh1YmcyuS4Dtki3LFHsh8KEUjUpR15.jpg?size=400x598&quality=96&crop=71,386,928,1388&ava=1",
     progress: [
       { _id: "67rdca3eeb7fprogress471818", percentages: 30 },
@@ -86,16 +106,12 @@ export const users = [
       { _id: "67rdca3eeb7progress471820", percentages: 91 },
       { _id: "67rdca3eeb7progress71824", percentages: 94 },
     ],
-    qualities: [
-      "67rdca3eeb7f6fgeed4729j01",
-      "67rdca3eeb7f6fgeed471101",
-      "67rdca3eeb7f6fgeed4711012",
-      "67rdca3eeb7f6fgeed47689",
-    ],
+    qualities: ["67rdca3eeb7f6fgeed4729j01", "67rdca3eeb7sjssh1992j01"],
   },
   {
     _id: "67rdca3eeb7f6fgeed471820",
     name: "Милес Виталий",
+    social: null,
     img: "https://avatars.githubusercontent.com/u/112376989?s=400&u=6cbbc19f57b52298d9ce1e6c566a0c0bea26036c&v=4",
     progress: [
       { _id: "67rdca3eeb7fprogress471818", percentages: 50 },
