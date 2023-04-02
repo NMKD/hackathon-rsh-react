d7215fa1cc7a505f0b64e68df75f2a2eded69988
kristina <kistina@localhost>
2023-04-02 14:03:43 +0300

    chnged cards, added button, links components

---
+++ src/components/common/Links.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+
+const Links = ({ id, children }) => {
+  return (
+    <Link
+      className="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+      to={`users/${id}`}
+    >
+      Открыть
+      <svg
+        aria-hidden="true"
+        className="w-4 h-4 ml-2 -mr-1"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fillRule="evenodd"
+          d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+          clipRule="evenodd"
+        ></path>
+      </svg>
+    </Link>
+  );
+};
+
+export default Links;
