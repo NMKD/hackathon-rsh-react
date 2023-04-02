9f8a8d445ce1bae900693bcf15616a279c331784
Anastasia Okladnaya <112321315+nastya-90@users.noreply.github.com>
2023-04-02 16:04:16 +0500

    add forms (#22)

---
+++ src/form/textField.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const TextField = ({ label, name, value, onChange, error }) => {
+  const handleChange = ({ target }) => {
+    onChange({ name: target.name, value: target.value });
+  };
+  const getInputClasses = () => {
+    return (
+      "shadow appearance-none border" +
+      (error ? " border-red-500" : "") +
+      "rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+    );
+  };
+
+  return (
+    <div className="mb-4">
+      <label
+        className="block text-gray-700 text-sm font-bold mb-2"
+        htmlFor={name}
+      >
+        {label}
+      </label>
+      <input
+        type="text"
+        id={name}
+        name={name}
+        value={value}
+        onChange={handleChange}
+        className={getInputClasses()}
+      />
+      {error && <div className="text-red-500 text-xs italic">{error}</div>}
+    </div>
+  );
+};
+TextField.defaultProps = {
+  type: "text",
+};
+TextField.propTypes = {
+  label: PropTypes.string,
+  type: PropTypes.string,
+  name: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  error: PropTypes.string,
+};
+
+export default TextField;
