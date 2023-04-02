67551922d67510a8c2d25b3dc4e0a293bd53c1fc
AlexLenk <alexlenk.ru@gmail.com>
2023-04-02 15:33:11 +0300

    Hotfix bugs

--- src/pages/userPage.jsx
+++ src/pages/userPage.jsx
@@ -1,6 +1,3 @@
-/* eslint-disable indent */
-/* eslint-disable function-call-argument-newline */
-/* eslint-disable comma-dangle */
 import React from "react";
 import Container from "../components/common/container";
 import Users from "../components/ui/Users/Users";
