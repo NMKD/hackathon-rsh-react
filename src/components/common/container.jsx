f92775665ff99291a156fa63f76903f7c658c514
Alexandr Yank <alexlenk.ru@gmail.com>
2023-04-02 15:36:04 +0300

    Hotfix bugs (#31)

--- src/components/common/container.jsx
+++ src/components/common/container.jsx
@@ -1,11 +1,11 @@
 import PropTypes from "prop-types";
 
 const Container = ({ children }) => {
-  return <div className="flex md:justify-between sm:px-12 px-2 py-7 mx-auto max-w-7xl">{children}</div>;
+  return <div className="md:justify-between sm:px-12 px-2 py-7 mx-auto container">{children}</div>;
 };
 
 Container.propTypes = {
-  children: PropTypes.string,
+  children: PropTypes.any,
 };
 
 export default Container;
